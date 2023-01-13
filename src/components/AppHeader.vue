b2cae61dcb0b9626ae2b1604090c248f91548873
dunzolo <rossidavide108@gmail.com>
2023-01-13 14:59:19 +0100

    added AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+
+}
+</script>
+
+<template lang="">
+    <header>
+        <img src="/ygoprodeck_header_logo.png" alt="">
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    height: 80px;
+    display: flex;
+    align-items: center;
+    padding: 0 3rem;
+
+    img {
+        margin-right: 3rem;
+    }
+}
+</style>
