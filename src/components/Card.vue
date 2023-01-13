a4385eb1eaf057cb53e1e45cff768a8061e32ade
dunzolo <rossidavide108@gmail.com>
2023-01-13 17:19:30 +0100

    added style cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,11 +7,30 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        <img :src="card.card_images[0].image_url_small" alt="">
+    <div class="card text-center">
+        <img :src="card.card_images[0].image_url" alt="">
+        <span class="text-light name-card">{{ card.name }}</span>
+        <span class="archetype">{{ card.archetype }}</span>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.card {
+    width: calc(100% / 5 - 20px);
+    background-color: #D48F38;
+    border: none;
+    margin-bottom: 1rem;
 
+    .name-card {
+        padding: 1rem 0;
+        text-transform: uppercase;
+        font-size: 18px;
+        font-weight: 700;
+    }
+
+    .archetype {
+        font-size: 15px;
+    }
+
+}
 </style>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -17,11 +17,14 @@ export default {
 </script>
 
 <template lang="">
-    <div class="container d-flex flex-wrap">
+    <div class="container d-flex justify-content-between flex-wrap">
         <Card v-for="(item, index) in store.cards_list" :key="index" :card="item"/>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+.container {
+    background-color: white;
+    padding: 2rem;
+}
 </style>
